9a776671d7b8e395c7de86471a5ac079f95b9c57
mahmoud-cr7 <you@example.comwageeh@DESKTOP-OLGM1G0>
2024-04-19 20:53:03 +0200

    enhance input in modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from "react";
 import ProductCard from "./commponets/ProductCard";
-import Modal from './commponets/ui/Modal';
-import { productList } from "./data";
+import Modal from "./commponets/ui/Modal";
+import { formInputsList, productList } from "./data";
 import Button from "./commponets/ui/Button";
+import Input from "./commponets/ui/Input";
 
 const App = () => {
   const renderProductList = productList.map((product) => (
     <ProductCard key={product.id} {...product} product={product} />
+  ));
+  const renderFormInputList = formInputsList.map((input) => (
+    <div className="flex flex-col">
+      <label
+        className="mb-2 text-sm font-medium text-gray-700"
+        htmlFor={input.id}
+      >
+        {input.label}
+      </label>
+      <Input type="text" id={input.id} name={input.name} />
+    </div>
   ));
   const [isOpen, setIsOpen] = useState(false);
 
@@ -28,10 +40,15 @@ const App = () => {
         {renderProductList}
       </div>
       <Modal isOpen={isOpen} closeModal={closeModal} title="Add New Product">
-        <div className="flex items-center justify-between space-x-3">
-          <Button className="bg-indigo-700 hover:bg-indigo-800">Submit</Button>
-          <Button className="bg-gray-300 hover:bg-gray-400">Close</Button>
-        </div>
+        <form className="space-y-3">
+          {renderFormInputList}
+          <div className="flex items-center justify-between space-x-3">
+            <Button className="bg-indigo-700 hover:bg-indigo-800">
+              Submit
+            </Button>
+            <Button className="bg-gray-400 hover:bg-gray-500">Close</Button>
+          </div>
+        </form>
       </Modal>
     </main>
   );
